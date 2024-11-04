22cc8237c9c406a9c7d4aa1e07844a94b7c75433
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-11-04 16:58:41 +0100

    add: slug on app.js

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const express= require("express")
-const app= express()
+const express = require("express")
+const app = express()
 
-const HOST= process.env.HOST
-const PORT= process.env.PORT
+const HOST = process.env.HOST
+const PORT = process.env.PORT
 
-app.listen(PORT, (req, res) =>{
+app.listen(PORT, (req, res) => {
     console.log(`Server is running at ${HOST}:${PORT}`);
 })
 
-app.get("/", (req, res) =>{
-    res.send("Siamo all'Index")
+const posts = require("./db.js")
+app.get("/", (req, res) => {
+    //res.send("Siamo all'Index")
+
+    res.status(200).json({
+        data: posts,
+    })
+})
+
+app.get("/:slug", (req, res) => {
+    //console.log(req.params.slug);
+
+    let post = posts.find(post => post.slug === (req.params.slug))
+    console.log(post);
+    if (!post) {
+        return res.status(404).json({
+            error: `404! Not found!`
+        })
+    }
+
+    res.json({
+        data: post
+    })
 })
